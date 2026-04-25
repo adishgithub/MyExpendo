64a198f999146c0315c98bb8b58ab46295af452d
adishprasad <adishsujaprasad@gmail.com>
2026-04-25 13:43:02 +0530

    fix: use npx vite for vercel build

--- web/src/pages/Login.jsx
+++ web/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
+import API from '../utils/api'
 import Input from '../components/Input'
 import Button from '../components/Button'
 import HeroPanel from '../components/HeroPanel'
